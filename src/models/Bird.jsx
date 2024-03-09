3ef1cca9b3f9c84fedde65bb8b5999e483c0f545
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-09 18:48:12 +0530

    bird on screen

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -5,7 +5,7 @@ import { useGLTF } from '@react-three/drei';
 const Bird = () => {
     const  {scene ,animations }= useGLTF(birdScene);
   return (
-    <mesh>
+    <mesh position={[-5,2,1]} scale={[0.003,0.003,0.003]}>
         <primitive object = {scene}/>
     </mesh>
   )
