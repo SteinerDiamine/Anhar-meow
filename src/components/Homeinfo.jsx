fb7be096ef0c86806e45ec48079f5c07684fb1f0
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-12 19:58:03 +0530

    Done with homepage

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+/*import React from 'react'
+import { Link } from 'react-router-dom'
+import {arrow} from '../assets/icons'
+
+const InfoBox = ({text , link, btnText}) => (
+    <div className='info-box'>
+        <p className='font-medium sm:xl text-center'>{text}</p>
+        <Link to= {Link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src = {arrow} className='w-4 h-4 object-contain'/>
+        </Link>
+    </div>
+)
 
 
 const renderContent = {
@@ -11,24 +23,29 @@ const renderContent = {
     ),
 
     2: (
-        <h1>2</h1>
+        <InfoBox
+        text ="Currently pursuing Bachelor's Degree"
+        link= "/about"
+        btnText="Learn more"/>
     ),
 
     3: (
-        <h1>3</h1>
+        <InfoBox
+        text ="I like Painting 🎨"
+        link= "/projects"
+        btnText="Visit my portfolio"/> 
     ),
 
     4: (
-        <h1>4</h1>
+        <InfoBox
+        text ="Reach me for more info"
+        link= "/contact"
+        btnText="Let's talk"/>
     ),
 
 }
 
-const InfoBox = ({text , link, btnText}) => {
-    <div>
-        {text}
-    </div>
-}
+
 
 const Homeinfo = ({currentStage}) => {
   return renderContent[currentStage] || null;
@@ -36,4 +53,75 @@ const Homeinfo = ({currentStage}) => {
   
 }
 
-export default Homeinfo
+export default Homeinfo */
+
+
+
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Anhar</span>
+        ฅ^•ﻌ•^ฅ
+        
+        <br />
+        <br/>
+        Looking forward to learn new things📚
+
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+        Currently pursuing Bachelor's Degree<br /> and picked up many skills along the way
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          I like painting 🎨
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        I am open and ready for new oppurtunities
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
