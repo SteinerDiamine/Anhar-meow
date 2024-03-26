334d07420d94649d9eeaa2005551b481e83304bf
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-26 06:28:57 +0530

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -37,8 +37,8 @@ const About = () => {
         <div className='mt-16 flex flex-wrap gap-12'>
           {skills.map((skill) =>(
             <div className='block-container w-20 h-20'>
-              <div className='btn-font rounded-xl flex justify-center items-center'/>
-              <div className='btn-front rounded-xl flex justify-center items-center'>
+              <div className='btn-font rounded-xl flex justify-center items-center ' />
+              <div className='btn-front rounded-xl flex justify-center items-center' style={{ backgroundColor: 'transparent' }}>
                 <img 
                 src={skill.imageUrl}
                 alt={skill.name}
