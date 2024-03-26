1b7e366bd1f20fc79f1b9ae7c61f4d0ec0595677
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-26 09:31:58 +0530

    commit

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -32,11 +32,7 @@ export const skills = [
         name: "CSS",
         type: "Frontend",
     },
-    {
-        imageUrl: express,
-        name: "Express",
-        type: "Backend",
-    },
+    
     {
         imageUrl: git,
         name: "Git",
@@ -57,46 +53,18 @@ export const skills = [
         name: "JavaScript",
         type: "Frontend",
     },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nextjs,
-        name: "Next.js",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
+  
+   
+  
+  
+   
     {
         imageUrl: react,
         name: "React",
         type: "Frontend",
     },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
-    },
-    {
-        imageUrl: sass,
-        name: "Sass",
-        type: "Frontend",
-    },
+  
+   
     {
         imageUrl: tailwindcss,
         name: "Tailwind CSS",
